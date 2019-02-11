4d3f033f8f4be90d4d2b88761a50b146f222ff79
hbarker <hc-barker@users.noreply.github.com>
2019-02-11 15:23:45 -0700

    Added post handling for the new user function

--- scripts/posthandle.js
+++ scripts/posthandle.js
@@ -15,4 +15,19 @@ app.get("/", function(request, response){
 	console.log("gotten");
 });
 
+app.post("/adduser", function(request, response){
+	response.send("Hello World");
+	console.log(request.body);
+	MongoClient.connect(url, function(err, db){
+		if(err) throw err;
+		console.log("Connected");
+		var dbo = db.db("test");
+		dbo.collection("members").insertOne(request.body, function(err, res) {
+			if (err) throw err;
+			console.log("1 document inserted");
+			db.close();
+		});
+	});
+});
+
 app.listen(3000);
