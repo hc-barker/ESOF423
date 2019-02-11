47742e02a530fd0b7b618693011daa52a6c66c3b
hbarker <hc-barker@users.noreply.github.com>
2019-02-11 14:56:24 -0700

    Added the initial node.js backend script

---
+++ scripts/posthandle.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+var jsonParser = bodyParser.json();
+
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27817/test";
+
+app.use(bodyParser.urlEncoded({extended: true}));
+
+app.get("/", function(request, response){
+	console.log("gotten");
+});
