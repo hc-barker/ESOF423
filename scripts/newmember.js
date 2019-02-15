f3971d7e5ddf75b7642ed67fd3e0d95adfe335dd
Ren Wall <renwall@protonmail.ch>
2019-02-15 13:52:01 -0700

    Fixed Directory Structure

--- scripts/newmember.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load",function() {
-	document.getElementById('new-member-form').addEventListener("submit",
-			function(e) {
-				e.preventDefault();
-				alert('submitted');
-			})
-});
