9bc07effc4f383575442998940bc24b5221a3444
Ren Wall <renwall@protonmail.ch>
2019-02-15 15:39:31 -0700

    Commented, normalised variable names, removed unused variables.

--- app/posthandle.js
+++ app/posthandle.js
@@ -1,3 +1,4 @@
+//External Requirements
 const http = require('http');
 const express = require("express");
 const myParser = require("body-parser");
@@ -14,26 +15,33 @@ const saltRounds = 10;
 
 app.use(myParser.urlencoded({extended: true}));
 
+//get handler for testing
 app.get("/", function(request, response){
 	console.log("gotten");
 });
 
+//post handler for adding members
 app.post("/addmember", function(request, response){
 	response.send("Hello World");
 	console.log(request.body);
-	MongoClient.connect(url, function(err, db){
+	MongoClient.connect(url, function(err, mongo){
 		if(err) throw err;
 		console.log("Connected");
-		var dbo = db.db("test");
-		dbo.collection("members").insertOne(request.body, function(err, res) {
+		mongo.db("test").collection("members").insertOne(request.body, function(err, res) {
 			if(err) throw err;
 			console.log("1 document inserted");
-			db.close();
+			mongo.close();
 		});
 	});
 });
 
-const addUserFailure = "Failed to add user";
+/*
+* post handler for adding users
+* 
+* Only allows a user to be added if there is no other user with the same username.
+* Encrypts passwords with bcrypt
+*/
+const addUserFailure = "Failed to add user"; //Success and failure messages
 const addUserSuccess = "Successfully added user";
 app.post("/adduser", function(request, response){
 	MongoClient.connect(url, function(err, mongo){
@@ -41,17 +49,17 @@ app.post("/adduser", function(request, response){
         	var table = mongo.db("test").collection("users");
 		table.findOne({username: request.body.username},function(err, user) {
 			if (err) throw err;
-			if (user != null) {
+			if (user != null) {//check if user with this name already exists
 				response.send(addUserFailure);
 			} 
-			else {
+			else { //hash password
 				bcrypt.hash(request.body.password, saltRounds, function(err, hash) {
 					if (err) throw err;
 					var newUser = {
 						username: request.body.username,
 						password: hash
 					};
-					table.insertOne(newUser, function(err, res) {
+					table.insertOne(newUser, function(err, res) { //insert user into db
                         			if(err) throw err;
                         			console.log("1 document inserted");
 						response.send(addUserSuccess);
@@ -62,18 +70,22 @@ app.post("/adduser", function(request, response){
 	});
 });
 
-const loginSuccess = "Logged in successfully";
+/*
+* post handler for verifying login attempts
+*
+* If the posted user exists and the hashed password matches allow them to log in.
+*/
+const loginSuccess = "Logged in successfully"; //success and failure messages
 const loginFailure = "Login failed, bad username or password.";
 app.post("/login", jsonParser, function(request, response) {
 	if(!request.body) return response.sendStatus(400);
 	console.log(request.body);
-	MongoClient.connect(url, function(err, db){
+	MongoClient.connect(url, function(err, mongo){
 		if (err) throw err;
 		console.log("Connected");
 		var usernameObject = {username: request.body.username};
 		console.log(usernameObject);
-		var dbo = db.db("test");
-		dbo.collection("users").findOne(usernameObject,function(err, user) {
+		mongo.db("test").collection("users").findOne(usernameObject,function(err, user) {
 			if (err) throw err;
 			console.log(user);
 			if(user != null){
@@ -91,20 +103,21 @@ app.post("/login", jsonParser, function(request, response) {
 			else {
 				response.send(loginFailure);
 			}
-			db.close();
+			mongo.close();
 		});
 	});
 });
 
+
+//post handler for purging member database
 app.post("/purge", function(request, response) {
-	MongoClient.connect(url, function(err, db){
+	MongoClient.connect(url, function(err, mongo){
 		if (err) throw err;
 		console.log("Connected");
-		var dbo = db.db("test");
-		dbo.collection("members").drop(function(err, delOK) {
+		mongo.db("test").collection("members").drop(function(err, delOK) {
 			if(err) throw err;
 			if(delOK) console.log("Member collection purged");
-			db.close();
+			mongo.close();
 		});
 	});
 });
