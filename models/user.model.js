254633024ff31461b85ba162b5e44734381a2bc0
hbarker <hc-barker@users.noreply.github.com>
2019-03-05 12:24:35 -0700

    Smal css change to list member, creating user schema

---
+++ models/user.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let UserSchema = newSchema(
+		{
+			username: {type: String, required: true, max:100},
+			password: {type: String, required: true}
+		});
+
+module.exports = mongoose.model('User', UserSchema);
