4ccbd656f1d7681c9b683e40203c2a4210aa8174
hbarker <hc-barker@users.noreply.github.com>
2019-03-04 21:20:15 -0700

    Setting up mongodb with the node server

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const bodyParser = require('body-parser');
 const member = require('./routes/member.route');
 const app = express();
 
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://localhost:27017';
+const mongoDB = dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/members', member);
 
 app.set('view engine', 'ejs');
