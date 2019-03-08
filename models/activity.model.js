085029554dc6fe0aea87488dd57537d4c9a17946
hbarker <hc-barker@users.noreply.github.com>
2019-03-08 11:37:00 -0700

    Updated activity schema

--- models/activity.model.js
+++ models/activity.model.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const member = require('./member.model');
 
 let ActivitySchema = new Schema(
 		{
 			name: {type: String, required: true, max: 100},
-			members: [String]
 		});
 
 module.exports = mongoose.model('Activity', ActivitySchema);
