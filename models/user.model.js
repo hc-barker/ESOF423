7e016d62b210ca245b311168df97a409386f1e1b
Ren Wall <renwall@protonmail.ch>
2019-03-11 10:29:50 -0600

    Add groups field to user model.

--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 let UserSchema = new Schema(
 		{
 			username: {type: String, required: true, trim:true, max:100},
-			password: {type: String, required: true}
+			password: {type: String, required: true},
+			groups:   [String]
 		});
 
 module.exports = mongoose.model('User', UserSchema);
