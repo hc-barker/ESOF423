fc7791b0673da94520ce667eaf03754f970b9dca
hbarker <hc-barker@users.noreply.github.com>
2019-04-12 20:40:39 -0600

    Improving test coverage

--- test/member_test.js
+++ test/member_test.js
@@ -11,23 +11,48 @@ const mockRequest = (sessionData, body) => ({
 describe('Members', function() {
 	describe('Member create page', function() {
 		it('renders a response with no activities', function() {
-			var server = require('../server');
+			//var server = require('../server');
 			//var spy = sinon.spy(ejs, '__express');
 			var req = mockRequest(
-					{},
-					{activities: ""}
+					{username: "test"},
+					{}
+					);
+
+			res = {};
+			res.render = sinon.spy();
+			
+			test.member_create_page(req, res);
+			expect(res.render.calledOnce).to.be.false;
+		});
+	});
+
+	describe('Document page', function() {
+		it('renders a response', function() {
+			var req = mockRequest(
+					{username: "test"},
+					{}
+					);
+
+			res = {};
+		  res.render = sinon.spy();
+
+			test.document_page(req, res);
+			expect(res.render.calledOnce).to.be.true;
+		});
+	});
+
+	describe('List member page', function() {
+		it('renders a response', function() {
+			var req = mockRequest(
+					{username: "test"},
+					{}
 					);
 
 			res = {};
 			res.render = sinon.spy();
 
-			test.member_create_page(req, res);
-			//expect(res.render.calledOnce).to.be.true;
-			expect(spy.calledWithMatch(/\/create_member\.ejs%/)).to.be.true;
-
-			//spy.restore();
-
-			done();
+			test.list_members(req, res);
+			expect(res.render.calledOnce).to.be.false;
 		});
 	});
 });
--- test/user_test.js
+++ test/user_test.js
@@ -3,6 +3,41 @@ const expect = chai.expect;
 const sinon = require('sinon');
 const test = require('../controllers/user.controller');
 
-describe('Users', function() {
+const mockRequest = (sessionData, body) => ({
+	session: {data: sessionData},
+	body,
+});	
 
+describe('Users', function() {
+	describe('User create page', function() {
+		it('renders a responso', function() {
+			var req = mockRequest(
+					{username: "test"},
+					{}
+					);
+
+			var res = {};
+			res.render = sinon.spy();
+
+			test.user_create(req, res);
+
+			expect(res.render.calledOnce).to.be.false;
+		});
+	});
+
+	describe('List user page', function() {
+		it('renders a respons', function() {
+			var req = mockRequest(
+					{username: "test"},
+					{}
+					);
+
+			var res = {};
+			res.render = sinon.spy();
+
+			test.list_users(req, res);
+
+			expect(res.render.calledOnce).to.be.false;
+		});
+	});
 });
